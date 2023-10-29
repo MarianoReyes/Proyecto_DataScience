81e47159db1b3a1c6f05b0c6b1dfc563f3034609
MarianoReyes <josemarianoreyesh02@gmail.com>
2023-10-29 13:01:57 -0600

    resultados

--- main_recomendaciones.py
+++ main_recomendaciones.py
@@ -3,8 +3,20 @@ import torch
 import torch.nn as nn
 from transformers import AutoTokenizer
 from transformers import AutoModelForSequenceClassification
+import pandas as pd
 
 app = Flask(__name__)
+
+# Cargar el archivo CSV en un DataFrame
+df = pd.read_csv('data/summaries_train.csv')
+
+# Encontrar el valor máximo y mínimo en la columna 'content'
+max_content = df['content'].max()
+min_content = df['content'].min()
+
+# Encontrar el valor máximo y mínimo en la columna 'wording'
+max_wording = df['wording'].max()
+min_wording = df['wording'].min()
 
 
 @app.route('/')
@@ -16,7 +28,12 @@ def home():
 def process_text_deberta():
     if request.method == 'POST':
         texto_de_prueba = request.form['input_text']
-        print(type(texto_de_prueba))
+
+        show_content_tips = False
+        show_success_content = False
+        show_wording_tips = False
+        show_success_wording = False
+
         # Tokenizar y ajustar la secuencia de entrada
 
         # Cargar el tokenizer
@@ -42,8 +59,29 @@ def process_text_deberta():
         predicted_content = probabilidades_clase[0][0].item()
         predicted_wording = probabilidades_clase[0][1].item()
 
-        # , show_content_tips=show_content_tips, show_wording_tips=show_wording_tips, show_success_content=show_success_content, show_success_wording=show_success_wording
-        return render_template('results_deberta.html', input_text=texto_de_prueba, content=predicted_content, wording=predicted_wording)
+        # Definir el nuevo número que deseas comparar
+        nuevo_numero_content = predicted_content
+        nuevo_numero_wording = predicted_wording
+
+        # Calcular el porcentaje para 'content'
+        predicted_content = (nuevo_numero_content -
+                             min_content) / (max_content - min_content)
+
+        # Calcular el porcentaje para 'wording'
+        predicted_wording = (nuevo_numero_wording -
+                             min_wording) / (max_wording - min_wording)
+
+        if predicted_content < 0.6:
+            show_content_tips = True
+        else:
+            show_success_content = True
+
+        if predicted_wording < 0.7:
+            show_wording_tips = True
+        else:
+            show_success_wording = True
+
+        return render_template('results_deberta.html', input_text=texto_de_prueba, content=predicted_content, wording=predicted_wording, show_content_tips=show_content_tips, show_wording_tips=show_wording_tips, show_success_content=show_success_content, show_success_wording=show_success_wording)
 
 
 @app.route('/process_text_lstm', methods=['POST'])
