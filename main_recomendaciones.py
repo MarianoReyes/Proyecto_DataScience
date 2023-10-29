4e5aa4c9011b9d932bc50f9bd3bf50297842bc04
MarianoReyes <josemarianoreyesh02@gmail.com>
2023-10-29 11:50:04 -0600

    changes
    This reverts commit d6ec564ec1a235f581886bdedf8184a237b1c0f9.

--- main_recomendaciones.py
+++ main_recomendaciones.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template
 import torch
 import torch.nn as nn
 from transformers import AutoTokenizer
+from transformers import AutoModelForSequenceClassification
 
 app = Flask(__name__)
 
@@ -14,13 +15,35 @@ def home():
 @app.route('/process_text_deberta', methods=['POST'])
 def process_text_deberta():
     if request.method == 'POST':
-        new_text = request.form['input_text']
+        texto_de_prueba = request.form['input_text']
+        print(type(texto_de_prueba))
         # Tokenizar y ajustar la secuencia de entrada
 
-        # aqui realizar la prediccion
+        # Cargar el tokenizer
+        tokenizer = AutoTokenizer.from_pretrained("models/debertav3base")
 
-        # content=predicted_content, wording=predicted_wording, show_content_tips=show_content_tips, show_wording_tips=show_wording_tips, show_success_content=show_success_content, show_success_wording=show_success_wording
-        return render_template('results_deberta.html', input_text=new_text)
+        # Crear una instancia del modelo
+        modelo_local = AutoModelForSequenceClassification.from_pretrained(
+            "models/debertav3base")
+
+        # Tokeniza el texto
+        tokens = tokenizer(texto_de_prueba, padding=True,
+                           truncation=True, max_length=512, return_tensors="pt")
+
+        # Realiza la predicción
+        with torch.no_grad():
+            input_ids = tokens["input_ids"]
+            attention_mask = tokens["attention_mask"]
+            prediction = modelo_local(input_ids, attention_mask)
+
+        # Obtener las probabilidades de clase
+        probabilidades_clase = torch.softmax(prediction.logits, dim=1)
+
+        predicted_content = probabilidades_clase[0][0].item()
+        predicted_wording = probabilidades_clase[0][1].item()
+
+        # , show_content_tips=show_content_tips, show_wording_tips=show_wording_tips, show_success_content=show_success_content, show_success_wording=show_success_wording
+        return render_template('results_deberta.html', input_text=texto_de_prueba, content=predicted_content, wording=predicted_wording)
 
 
 @app.route('/process_text_lstm', methods=['POST'])
--- try.py
+++ try.py
@@ -1,35 +1,59 @@
+import pandas as pd
 import torch
 from transformers import AutoTokenizer
 from transformers import AutoModelForSequenceClassification
 
 # Cargar el tokenizer
-tokenizer = AutoTokenizer.from_pretrained("models/debertav3base")
+tokenizer = AutoTokenizer.from_pretrained("models/deberta")
 
 # Crear una instancia del modelo
 modelo_local = AutoModelForSequenceClassification.from_pretrained(
-    "models/debertav3base")
+    "models/deberta")
 
-# Texto de entrada para hacer una predicción (reemplaza con tu propio texto)
-texto_de_prueba = "The Third Wave was an experiment to see how people reacted to a new one leader government."
 
-# Tokeniza el texto
-tokens = tokenizer(texto_de_prueba, padding=True,
-                   truncation=True, max_length=512, return_tensors="pt")
+# Cargar el archivo CSV
+df = pd.read_csv("data/summaries_train.csv")
 
-# Realiza la predicción
-with torch.no_grad():
-    input_ids = tokens["input_ids"]
-    attention_mask = tokens["attention_mask"]
-    prediction = modelo_local(input_ids, attention_mask)
+# Crear listas para almacenar los resultados
+textos = []
+contents = []
+wordings = []
+probabilidades_clase_0 = []
+probabilidades_clase_1 = []
 
-# Obtener las probabilidades de clase
-probabilidades_clase = torch.softmax(prediction.logits, dim=1)
+# Iterar sobre las primeras 10 filas del DataFrame
+for index, row in df.head(100).iterrows():
+    # Obtener el texto de la fila
+    texto_de_prueba = row["text"]
 
-# Ahora puedes acceder a las probabilidades para cada clase
-# Probabilidad de la clase 0
-probabilidad_clase_0 = probabilidades_clase[0][0].item()
-# Probabilidad de la clase 1
-probabilidad_clase_1 = probabilidades_clase[0][1].item()
+    # Tokeniza el texto
+    tokens = tokenizer(texto_de_prueba, padding=True,
+                       truncation=True, max_length=512, return_tensors="pt")
 
-print("Probabilidad de la clase 0:", probabilidad_clase_0)
-print("Probabilidad de la clase 1:", probabilidad_clase_1)
+    # Realiza la predicción
+    with torch.no_grad():
+        input_ids = tokens["input_ids"]
+        attention_mask = tokens["attention_mask"]
+        prediction = modelo_local(input_ids, attention_mask)
+
+    # Obtener las probabilidades de clase
+    probabilidades_clase = torch.softmax(prediction.logits, dim=1)
+
+    # Guardar los resultados
+    textos.append(row["text"])
+    contents.append(row["content"])
+    wordings.append(row["wording"])
+    probabilidades_clase_0.append(probabilidades_clase[0][0].item())
+    probabilidades_clase_1.append(probabilidades_clase[0][1].item())
+
+# Crear un nuevo DataFrame con los resultados
+resultados_df = pd.DataFrame({
+    "text": textos,
+    "content": contents,
+    "wording": wordings,
+    "probabilidad_clase_0": probabilidades_clase_0,
+    "probabilidad_clase_1": probabilidades_clase_1
+})
+
+# Guardar los resultados en un nuevo archivo CSV
+resultados_df.to_csv("data/resultados_deberta.csv", index=False)
